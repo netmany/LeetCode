aae69eea97e99543cc39e6d5d3db955b18df1316
netone <netreader@163.com>
2014-09-27 14:51:29 +0800

    Create Reverse Nodes in k-Group.cpp

---
+++ Lists/Reverse Nodes in k-Group.cpp
@@ -0,0 +1,56 @@
+/*
+Given a linked list, reverse the nodes of a linked list k at a time and return its modified list.
+
+If the number of nodes is not a multiple of k then left-out nodes in the end should remain as it is.
+
+You may not alter the values in the nodes, only nodes itself may be changed.
+
+Only constant memory is allowed.
+
+For example,
+Given this linked list: 1->2->3->4->5
+
+For k = 2, you should return: 2->1->4->3->5
+
+For k = 3, you should return: 3->2->1->4->5
+*/
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *reverseKGroup(ListNode *head, int k) {
+        if(k == 0) return head;
+        ListNode h(0), *tail = &h, *second = head, *pre, *temp;
+        int count;
+        while(second){
+            count = 0; 
+            temp = second;
+            while(temp != NULL && count < k){
+                count++; 
+                pre = temp;
+                temp =temp->next;
+            }
+            if(count < k){
+                tail->next = second; break;
+            } else {
+                pre->next = NULL;
+                ListNode *t = second, *p;
+                while(t != NULL){
+                    p = t->next; t->next = tail->next; tail->next = t;
+                    t = p;
+                }
+                tail = second; tail->next = NULL;
+                second = temp;
+            }
+        }
+        return h.next;
+    }
+};
