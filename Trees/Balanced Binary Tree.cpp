83636885da01923341373a326b350bc82b828849
netone <netreader@163.com>
2014-09-27 16:58:28 +0800

    Create Balanced Binary Tree.cpp

---
+++ Trees/Balanced Binary Tree.cpp
@@ -0,0 +1,33 @@
+/*
+Given a binary tree, determine if it is height-balanced.
+
+For this problem, a height-balanced binary tree is defined as a binary tree 
+in which the depth of the two subtrees of every node never differ by more than 1.
+*/
+
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+    int height(TreeNode *root){
+        if (root == NULL)
+            return 0;
+        else
+            return 1 + max(height(root->left), height(root->right));
+    }
+public:
+    bool isBalanced(TreeNode *root) {
+        if (root == NULL)
+            return true;
+        else
+            return abs(height(root->left) - height(root->right)) < 2 &&
+                isBalanced(root->left) &&
+                isBalanced(root->right);
+    }
+};
