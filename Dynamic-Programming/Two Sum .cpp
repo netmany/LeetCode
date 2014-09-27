a82a13dd2e72d87a09f9b8e4a048d9f6eaa97d3e
netone <netreader@163.com>
2014-09-27 14:11:56 +0800

    Create Two Sum .cpp

---
+++ Dynamic-Programming/Two Sum .cpp
@@ -0,0 +1,29 @@
+/*
+Given an array of integers, find two numbers such that they add up to a specific target number.
+
+The function twoSum should return indices of the two numbers such that they add up to the target, where index1 must be less than index2. Please note that your returned answers (both index1 and index2) are not zero-based.
+
+You may assume that each input would have exactly one solution.
+
+Input: numbers={2, 7, 11, 15}, target=9
+Output: index1=1, index2=2
+*/
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int> &numbers, int target) {
+        vector<int> res;
+        map<int, int> m;
+        for(int i = 0; i < numbers.size(); i++){
+            if(m.count(numbers[i]) == 0) {
+                m[target - numbers[i]] = i + 1;
+            } else {
+                res.push_back(m[numbers[i]]);
+                res.push_back(i + 1);
+                break;
+            }
+        }
+        if(!res.empty()) sort(res.begin(), res.end());
+        return res;
+    }
+};
