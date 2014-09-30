5ee2f6fe036344afb6d87de0137a2fe7f405ee41
netone <netreader@163.com>
2014-09-30 22:23:19 +0800

    Update Search in Rotated Sorted Array .cpp

--- Arrays/Search in Rotated Sorted Array .cpp
+++ Arrays/Search in Rotated Sorted Array .cpp
@@ -12,16 +12,24 @@ You may assume no duplicate exists in the array.
 class Solution {
 public:
     int search(int A[], int n, int target) {
-        if(target == A[0]){
-            return 0;
-        }else if(target > A[0]){
-            int i = 0;
-            while(A[i] != target && i+1 < n && A[i] <= A[i+1]) i++;
-            return A[i] == target ? i : -1;
-        } else {
-            int i = n - 1;
-            while(A[i] != target && i-1 >= 0 && A[i] >= A[i-1]) i--;
-            return A[i] == target ? i : -1;
+        int left = 0, right = n - 1, mid;
+        while(left <= right){
+            mid = (left + right) / 2;
+            if(A[mid] == target) return mid;
+            if(A[mid] < target){
+                if(A[left] <= A[mid]) left = mid + 1;
+                else {
+                    if(A[left] <= target) right = mid - 1;
+                    else left = mid + 1;
+                }
+            } else {
+                if(A[right] >= A[mid]) right = mid - 1;
+                else {
+                    if(A[left] <= target) right = mid - 1;
+                    else left = mid + 1;
+                }
+            }
         }
+        return -1;
     }
 };
